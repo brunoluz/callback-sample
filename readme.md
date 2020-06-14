05912953998e7bf4b79e764b23af7e43a7e2280c
Bruno Luz <bruno.luz@gmail.com>
2020-06-13 22:22:12 -0300

    swagger

--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
-create a service for linux: https://swimburger.net/blog/dotnet/how-to-run-aspnet-core-as-a-service-on-linux
+project page:
+- https://localhost:5001/swagger/index.html
 
+create a service for linux: 
+- https://swimburger.net/blog/dotnet/how-to-run-aspnet-core-as-a-service-on-linux
+
+add swagger into webapp:
+- https://docs.microsoft.com/pt-br/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-3.1&tabs=visual-studio
--- src/Startup.cs
+++ src/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
 
 namespace callback_sample
 {
@@ -26,11 +28,23 @@ namespace callback_sample
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Long Running Task API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Long Running Task API");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
